d01bdf0f8f8e775c41ae35a25c1c71d197237200
oceaster <oceaster@gmail.com>
2021-03-03 09:41:19 +0000

    major #1

--- global/src/apps/header/header.tsx
+++ global/src/apps/header/header.tsx
@@ -3,7 +3,7 @@ import './header.css';
 const Header = () => {
   return <div id='header'>
     <h1> App Browser </h1>
-    <h6> select an application from the list </h6>
+    select an application
   </div>
 }
 
