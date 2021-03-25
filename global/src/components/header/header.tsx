27c27c30a5ac59829fd678b5fea34c4ed8294c64
oceaster <oceaster@gmail.com>
2021-03-25 01:48:59 +0000

    fixed logged in user elements not showing by removing reactsnap from global postbuild

--- global/src/components/header/header.tsx
+++ global/src/components/header/header.tsx
@@ -3,26 +3,39 @@ import LoginBtn from '../../shared/components/user-btns/login-btn';
 import SignupBtn from '../../shared/components/user-btns/signup-btn';
 import ProfileBtn from '../../shared/components/user-btns/profile-btn';
 import MenuBtns from '../../shared/components/user-btns/menu-btns';
+import isUser from '../../shared/library/user/fetchData';
+
+
+const HeaderLeft = () => {
+  const InnerContent = () => {
+    if (isUser()) return <ProfileBtn />
+    return <SignupBtn />
+  }
+  return <div id='header-left'>
+    <InnerContent />
+  </div>
+}
+
+
+const HeaderRight = () => {
+  const InnerContent = () => {
+    if (isUser()) return <MenuBtns />
+    return <LoginBtn />
+  }
+  return <div id='header-right'>
+    <InnerContent />
+  </div>
+}
 
 
 const Header = () => {
   return <div id='header'>
-
-    <div id='header-left'>
-      <SignupBtn />
-      <ProfileBtn />
-    </div>
-
+    <HeaderLeft />
     <div id='header-title'>
       <h1> App Browser </h1>
       select an application
     </div>
-
-    <div id='header-right'>
-      <LoginBtn />
-      <MenuBtns />
-    </div>
-
+    <HeaderRight />
   </div>
 }
 
