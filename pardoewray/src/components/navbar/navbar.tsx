725ddd1a4f3f004d277f98dd17aaa5da63841edc
oceaster <oceaster@gmail.com>
2021-03-31 02:37:51 +0100

    BUILD 31 MAR 21

--- pardoewray/src/components/navbar/navbar.tsx
+++ pardoewray/src/components/navbar/navbar.tsx
@@ -1,16 +1,35 @@
 import './navbar.css';
 
+const navMenuOpen = () => {
+  const navbar = document.querySelector('#navbar') as HTMLDivElement;
+  const navbtn = document.querySelector('#nav-menu-button') as HTMLDivElement;
+  navbar.className = 'navbar-menu-open';
+  navbtn.className = 'nav-menu-button-open';
+}
+
+const navMenuClose = () => {
+  const navbar = document.querySelector('#navbar') as HTMLDivElement;
+  const navbtn = document.querySelector('#nav-menu-button') as HTMLDivElement;
+  navbar.className = 'navbar-menu';
+  navbtn.className = 'nav-menu-button';
+}
+
 const Navbar = () => {
   return <div id="nav-header">
     <div id="header" />
-    <div id="navbar">
+    <div id="navbar" className="navbar-menu" onClick={navMenuClose}>
       <button> Home </button>
       <button> Client </button>
       <button> Candidate </button>
       <button> About Us </button>
     </div>
-    <div id="nav-menu-button" />
+    <div
+      id="nav-menu-button"
+      className="nav-menu-button"
+      onClick={navMenuOpen}
+    />
   </div>
 };
+
 
 export default Navbar;
