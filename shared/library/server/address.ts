2c52b414773e8beb596f21ab77d3c2df44ace81e
oceaster <oceaster@gmail.com>
2021-05-08 16:50:22 +0100

    08 MAY 2021

--- shared/library/server/address.ts
+++ shared/library/server/address.ts
@@ -1,14 +1,27 @@
+import { appName } from '../../../routes';
 
-const getServerAdr = () => {
-  if (window.location.host.startsWith('localhost')){
+
+export const getServerAdr = () => {
+  // Standalone Django Server
+  if (window.location.host.endsWith(':3000')) {
+    return 'http://localhost:3000';
+  }
+  // Child Django Server
+  else if (window.location.host.startsWith('localhost')){
     return 'http://localhost:8000/';
-  } else if (window.location.host.startsWith('192.168.')) {
+  }
+  // Local Network React Application
+  else if (window.location.host.startsWith('192.168.')) {
     return window.location.host.split(':')[0] + ':8000';
-  } else {
+  }
+  // Public Domian
+  else {
     return 'https://' + window.location.host + '/';
   }
 }
 
+
+export const serverAPI = getServerAdr() + 'api/';
 
 const serverAdr = getServerAdr();
 
